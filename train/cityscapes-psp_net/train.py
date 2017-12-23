6fb8e926dd9fbc79dbfa64057a2c2c1a0b54a2af
Jian Cui <cuijian2015@qq.com>
2017-12-23 13:25:23 +0800

    Fixed the bug that val batch size = 1 can't run.

--- train/cityscapes-psp_net/train.py
+++ train/cityscapes-psp_net/train.py
@@ -1,7 +1,7 @@
 import datetime
 import os
 from math import sqrt
-
+#os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 import numpy as np
 import torchvision.transforms as standard_transforms
 from tensorboardX import SummaryWriter
@@ -16,17 +16,18 @@ from models import *
 from models.pspnet import PSPNet1
 from utils import check_mkdir, evaluate, AverageMeter, CrossEntropyLoss2d
 
-ckpt_path = '/media/cj/Elements/ckpt'
+ckpt_path = '../../ckpt'
 exp_name = 'cityscapes-psp_net'
 writer = SummaryWriter(os.path.join(ckpt_path, 'exp', exp_name))
 
 args = {
-    'train_batch_size': 2,
+    'train_batch_size': 1,
+    'val_batch_size': 1,
     'lr': 1e-2 / sqrt(16 / 2),
     'lr_decay': 0.9,
     'max_iter': 9e4,
     'longer_size': 2048,
-    'crop_size': 384,
+    'crop_size': 712,
     'stride_rate': 2 / 3.,
     'weight_decay': 1e-4,
     'momentum': 0.9,
@@ -85,7 +86,7 @@ def main():
     train_loader = DataLoader(train_set, batch_size=args['train_batch_size'], num_workers=8, shuffle=True)
     val_set = cityscapes.CityScapes('fine', 'val', transform=val_input_transform, sliding_crop=sliding_crop,
                                     target_transform=target_transform)
-    val_loader = DataLoader(val_set, batch_size=1, num_workers=8, shuffle=False)
+    val_loader = DataLoader(val_set, batch_size=args['val_batch_size'], num_workers=8, shuffle=False)
 
     criterion = CrossEntropyLoss2d(size_average=True, ignore_index=cityscapes.ignore_label).cuda()
 
@@ -111,7 +112,7 @@ def main():
 def train(train_loader, net, criterion, optimizer, curr_epoch, train_args, val_loader, visualize):
     while True:
         train_main_loss = AverageMeter()
-        #train_aux_loss = AverageMeter()
+        train_aux_loss = AverageMeter()
         curr_iter = (curr_epoch - 1) * len(train_loader)
         for i, data in enumerate(train_loader):
             optimizer.param_groups[0]['lr'] = 2 * train_args['lr'] * (1 - float(curr_iter) / train_args['max_iter']
@@ -133,6 +134,9 @@ def train(train_loader, net, criterion, optimizer, curr_epoch, train_args, val_l
 
                 optimizer.zero_grad()
                 outputs, aux = net(inputs_slice)
+                #print 'outputs ',outputs.size()
+                #print 'aux ', aux.size()
+                #print 'gt ',gts_slice.size()
                 #print outputs.size()
                 #print gts_slice.size()
                 #print cityscapes.num_classes
@@ -140,32 +144,33 @@ def train(train_loader, net, criterion, optimizer, curr_epoch, train_args, val_l
                 assert outputs.size()[1] == cityscapes.num_classes
 
                 main_loss = criterion(outputs, gts_slice)
-                #aux_loss = criterion(aux, gts_slice)
-                loss = main_loss# + 0.4 * aux_loss
+                aux_loss = criterion(aux, gts_slice)
+                loss = main_loss + 0.4 * aux_loss
                 loss.backward()
                 optimizer.step()
 
                 train_main_loss.update(main_loss.data[0], slice_batch_pixel_size)
-                #train_aux_loss.update(aux_loss.data[0], slice_batch_pixel_size)
+                train_aux_loss.update(aux_loss.data[0], slice_batch_pixel_size)
 
             curr_iter += 1
             writer.add_scalar('train_main_loss', train_main_loss.avg, curr_iter)
-            #writer.add_scalar('train_aux_loss', train_aux_loss.avg, curr_iter)
+            writer.add_scalar('train_aux_loss', train_aux_loss.avg, curr_iter)
             writer.add_scalar('lr', optimizer.param_groups[1]['lr'], curr_iter)
 
+            if (i + 1) % train_args['print_freq'] == 0:
+                print('[epoch %d], [iter %d / %d], [train main loss %.5f], [train aux loss %.5f]. [lr %.10f]' % (
+                    curr_epoch, i + 1, len(train_loader), train_main_loss.avg, train_aux_loss.avg,
+                    optimizer.param_groups[1]['lr']))
             #if (i + 1) % train_args['print_freq'] == 0:
-            #    print('[epoch %d], [iter %d / %d], [train main loss %.5f], [train aux loss %.5f]. [lr %.10f]' % (
+            #    print('[epoch %d], [iter %d / %d], [train main loss %.5f], [lr %.10f]' % (
             #        curr_epoch, i + 1, len(train_loader), train_main_loss.avg, #train_aux_loss.avg,
             #        optimizer.param_groups[1]['lr']))
-            if (i + 1) % train_args['print_freq'] == 0:
-                print('[epoch %d], [iter %d / %d], [train main loss %.5f], [lr %.10f]' % (
-                    curr_epoch, i + 1, len(train_loader), train_main_loss.avg, #train_aux_loss.avg,
-                    optimizer.param_groups[1]['lr']))
 
             if curr_iter >= train_args['max_iter']:
                 return
-            if curr_iter % train_args['val_freq'] == 0:
-                validate(val_loader, net, criterion, optimizer, curr_epoch, i + 1, train_args, visualize)
+            #if curr_iter % train_args['val_freq'] == 0:
+            #validate(val_loader, net, criterion, optimizer, curr_epoch, i + 1, train_args, visualize)
+        validate(val_loader, net, criterion, optimizer, curr_epoch, 0, train_args, visualize)
         curr_epoch += 1
 
 
@@ -189,18 +194,39 @@ def validate(val_loader, net, criterion, optimizer, epoch, iter_num, train_args,
         output = torch.zeros(cityscapes.num_classes, args['longer_size'] / 2, args['longer_size']).cuda()
 
         slice_batch_pixel_size = input.size(1) * input.size(3) * input.size(4)
-
+        #print 'input, gt, slices_info'
+        #print input.size()
+        #print gt.size()
+        #print slices_info.size()
         for input_slice, gt_slice, info in zip(input, gt, slices_info):
             input_slice = Variable(input_slice).cuda()
             gt_slice = Variable(gt_slice).cuda()
 
-            output_slice = net(input_slice)
+            output_slice, _  = net(input_slice)
             assert output_slice.size()[2:] == gt_slice.size()[1:]
             assert output_slice.size()[1] == cityscapes.num_classes
-            output[:, info[0]: info[1], info[2]: info[3]] += output_slice[0, :, :info[4], :info[5]].data
-            gts_all[vi, info[0]: info[1], info[2]: info[3]] += gt_slice[0, :info[4], :info[5]].data.cpu().numpy()
+            #print 'output.size ', output.size()
+            #print 'output_slice.size ', output_slice.size()
+            #print 'info0 ', info[0]
+            #print 'info1 ', info[1]
+            #print 'info2 ', info[2]
+            #print 'info3 ', info[3]
+            #print 'info4 ', info[4]
+            #print 'info5 ', info[5]
+            #print 'info ', info
+            #aaa = np.array(range(2000 * 2000)).reshape((2000,2000))
+            #ccc = aaa[info[0]: info[1]]
+            #print 'ccc = ', ccc
+            if(len(info.size())<2):
+                info = info.view(1,6)
 
-            count[info[0]: info[1], info[2]: info[3]] += 1
+            for i in range(info.size()[0]):
+                #print output.size()
+                #print info.size()
+                output[:, info[i,0]: info[i,1], info[i,2]: info[i,3]] += output_slice[0, :, :info[i,4], :info[i,5]].data
+                output[:, info[i,0]: info[i,1], info[i,2]: info[i,3]] += output_slice[0, :, :info[i,4], :info[i,5]].data
+                gts_all[vi, info[i,0]: info[i,1], info[i,2]: info[i,3]] += gt_slice[0, :info[i,4], :info[i,5]].data.cpu().numpy()
+                count[info[i,0]: info[i,1], info[i,2]: info[i,3]] += 1
 
             val_loss.update(criterion(output_slice, gt_slice).data[0], slice_batch_pixel_size)
 
@@ -209,7 +235,6 @@ def validate(val_loader, net, criterion, optimizer, epoch, iter_num, train_args,
         predictions_all[vi, :, :] = output.max(0)[1].squeeze_(0).cpu().numpy()
 
         print('validating: %d / %d' % (vi + 1, len(val_loader)))
-
     acc, acc_cls, mean_iu, fwavacc = evaluate(predictions_all, gts_all, cityscapes.num_classes)
     if val_loss.avg < train_args['best_record']['val_loss']:
         train_args['best_record']['val_loss'] = val_loss.avg
@@ -237,9 +262,10 @@ def validate(val_loader, net, criterion, optimizer, epoch, iter_num, train_args,
             gt_pil.save(os.path.join(to_save_dir, '%d_gt.png' % idx))
             val_visual.extend([visualize(gt_pil.convert('RGB')),
                                visualize(predictions_pil.convert('RGB'))])
-    val_visual = torch.stack(val_visual, 0)
-    val_visual = vutils.make_grid(val_visual, nrow=2, padding=5)
-    writer.add_image(snapshot_name, val_visual)
+    if(not (len(val_visual) == 0)):
+        val_visual = torch.stack(val_visual, 0)
+        val_visual = vutils.make_grid(val_visual, nrow=2, padding=5)
+        writer.add_image(snapshot_name, val_visual)
 
     print('-----------------------------------------------------------------------------------------------------------')
     print('[epoch %d], [iter %d], [val loss %.5f], [acc %.5f], [acc_cls %.5f], [mean_iu %.5f], [fwavacc %.5f]' % (
